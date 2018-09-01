49c240692c5ece0842744792b542e278b2e20a68
existencer <2680383605@qq.com>
2018-09-01 16:02:35 +0800

    ⚙️ Update logo

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 <style lang="stylus">
+body
+  margin 0
+
 #app
   font-family 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
   text-align center
   color #50382c
+  display flex
+  justify-content center
+  align-items center
+  margin 0
+  height 100vh
+  width 100vw
 
 #nav
   padding 30px
--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="logo-container">
-    <div class="left-piece" :style="{
+    <div class="lr-piece" :style="{
       'z-index' : angle % 180 - 90 > 45 || angle % 180 - 90 < -45 ? 0 : 1,
       'transform': `translate3d(-50%, -50%, 0) rotateX(-35.2643deg) rotateY(${angle % 180 - 90}deg) translateZ(50px)`
-    }"/>
-    <div class="right-piece" :style="{
+    }"><div :style="{ 'opacity': alphaLeft }"/></div>
+    <div class="lr-piece" :style="{
       'transform': `translate3d(-50%, -50%, 0) rotateX(-35.2643deg) rotateY(${(angle + 90) % 180 - 90}deg) translateZ(50px)`
-    }"/>
-    <div class="top-piece-mask" :style="{
-      'z-index': 91,
+    }"><div :style="{ 'opacity': alphaRight }"/></div>
+    <div :style="{
       'transform': `translate3d(-50%, -50%, 0) rotateX(54.7357deg) rotateZ(${-angle}deg) translateZ(50px)`
     }"/>
     <div class="top-piece" :style="{
-      'z-index': 91,
       'transform': `translate3d(-50%, -50%, 0) rotateX(54.7357deg) rotateZ(${-angle}deg) translateZ(${50 + offsetTop}px)`
     }"/>
   </div>
@@ -23,17 +21,19 @@ export default {
   name: 'Logo',
   data () {
     return {
-      angle: 0,
+      angle: 45,
       offsetTop: 0,
-      step: 0.4
+      step: 0.3
     }
   },
   computed: {
-    sinA () {
-      return Math.sin(this.angle * Math.PI / 120)
+    alphaLeft () {
+      const alpha = (this.angle % 180 - 45) / 90
+      return Math.max(Math.min(alpha, 1), 0)
     },
-    cosA () {
-      return Math.cos(this.angle * Math.PI / 120)
+    alphaRight () {
+      const alpha = ((this.angle + 90) % 180 - 45) / 90
+      return Math.max(Math.min(alpha, 1), 0)
     }
   },
   mounted () {
@@ -55,17 +55,18 @@ export default {
   width 200px
   height 200px
   margin auto
-  perspective 400px
-.logo-container div
+.logo-container>div
   position absolute
   width 50%
   height 50%
   top 50%
   left 50%
-.left-piece
+.lr-piece
   background-color #ffd633
-.right-piece
-  background-color #ffb81f
+  div
+    width 100%
+    height 100%
+    background-color #ffb81f
 .top-piece-mask
   background-color #ffffff
 .top-piece

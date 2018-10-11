06d3b7deda1368538bb6138d77eb4a70a24e9550
Felix <2680383605@qq.com>
2018-10-11 18:22:27 +0800

    🦕 add jump action

--- src/views/games/Dino.vue
+++ src/views/games/Dino.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="dino-scene">
-    <div id="dino-player">
+    <div id="dino-road">
+      <frame :asset="road">
+    </div>
+    <div id="dino-player" :style="{
+      'transform': `translateY(${-dinoJumpHeight}px)`
+    }">
       <frame v-for="(url, name, index) in this.dino" :key="index"
         v-if="dinoStatus === name" :asset="url" />
     </div>
@@ -10,25 +15,42 @@
 <script>
 import Frame from './Frame'
 
+const MAX_JUMP_HEIGHT = 94
+
+const NO_JUMP = 0
+const JUMP_UP = 1
+const JUMP_DOWN = 2
+
 const dinoAssets = {
   default: require('@/assets/dino/dino_default.png'),
   leftLeg: require('@/assets/dino/dino_left_leg.png'),
   rightLeg: require('@/assets/dino/dino_right_leg.png')
+}
+const road = {
+  road: require('@/assets/dino/road.png')
 }
 
 export default {
   name: 'Dino',
   data () {
     return {
+      raf: 0,
+      lastFrame: 0,
+
       assets: {
-        ...dinoAssets
+        ...dinoAssets,
+        ...road
       },
       dino: {
         default: '',
         leftLeg: '',
         rightLeg: ''
       },
-      dinoStatus: 'default'
+      road: '',
+
+      dinoStatus: 'default',
+      dinoJumping: NO_JUMP,
+      dinoJumpHeight: 0
     }
   },
   components: {
@@ -51,6 +73,51 @@ export default {
       for (const key in this.dino) {
         this.dino[key] = this.assets[key]
       }
+      this.road = this.assets['road']
+
+      this.bindEventListener()
+
+      this.raf = requestAnimationFrame(this.render)
+    },
+    bindEventListener () {
+      window.addEventListener('keydown', e => {
+        switch (e.keyCode) {
+          case 32:
+            return this.dinoJump()
+        }
+      })
+    },
+    render (timer) {
+      let delta = timer - this.lastFrame
+      if (delta > 100) {
+        delta = 0
+      }
+      this.lastFrame = timer
+      if (this.dinoJumping === JUMP_UP) {
+        this.dinoJumpHeight += MAX_JUMP_HEIGHT / 300 * delta
+        if (this.dinoJumpHeight > MAX_JUMP_HEIGHT) {
+          this.dinoJumpHeight = MAX_JUMP_HEIGHT
+          this.dinoJumping = JUMP_DOWN
+          this.dinoStatus = 'rightLeg'
+        }
+      } else if (this.dinoJumping === JUMP_DOWN) {
+        this.dinoJumpHeight -= MAX_JUMP_HEIGHT / 300 * delta
+        if (this.dinoJumpHeight < 0) {
+          this.dinoJumpHeight = 0
+          this.dinoJumping = NO_JUMP
+          this.dinoStatus = 'default'
+        }
+      }
+      this.raf = requestAnimationFrame(this.render)
+    },
+    dinoJump () {
+      this.dinoJumping = JUMP_UP
+      this.dinoStatus = 'leftLeg'
+    }
+  },
+  beforeDestroy () {
+    if (this.raf) {
+      cancelAnimationFrame(this.raf)
     }
   }
 }
@@ -62,10 +129,17 @@ export default {
   width 600px
   height 200px
   border solid 1px #ddd
+  overflow hidden
+#dino-road
+  position absolute
+  width 2400px
+  height 30px
+  left 0
+  bottom 20px
 #dino-player
   position absolute
   width 88px
   height 94px
   bottom 20px
-  left 40px
+  left 28px
 </style>

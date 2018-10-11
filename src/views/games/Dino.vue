8bce7e89352d372aa911af46f92d394bffd97185
Felix <2680383605@qq.com>
2018-10-11 12:05:02 +0800

    🎨 add cute little dinosaur

--- src/views/games/Dino.vue
+++ src/views/games/Dino.vue
@@ -1,11 +1,71 @@
 <template>
   <div id="dino-scene">
-    Waiting...
+    <div id="dino-player">
+      <frame v-for="(url, name, index) in this.dino" :key="index"
+        v-if="dinoStatus === name" :asset="url" />
+    </div>
   </div>
 </template>
 
 <script>
+import Frame from './Frame'
+
+const dinoAssets = {
+  default: require('@/assets/dino/dino_default.png'),
+  leftLeg: require('@/assets/dino/dino_left_leg.png'),
+  rightLeg: require('@/assets/dino/dino_right_leg.png')
+}
+
 export default {
-  name: 'Dino'
+  name: 'Dino',
+  data () {
+    return {
+      assets: {
+        ...dinoAssets
+      },
+      dino: {
+        default: '',
+        leftLeg: '',
+        rightLeg: ''
+      },
+      dinoStatus: 'default'
+    }
+  },
+  components: {
+    Frame
+  },
+  created () {
+    const loadAssets = []
+    for (const key in this.assets) {
+      const img = new Image()
+      loadAssets.push(new Promise((resolve, reject) => {
+        img.onload = resolve
+        img.onerror = reject
+      }))
+      img.src = this.assets[key]
+    }
+    Promise.all(loadAssets).then(this.init)
+  },
+  methods: {
+    init () {
+      for (const key in this.dino) {
+        this.dino[key] = this.assets[key]
+      }
+    }
+  }
 }
 </script>
+
+<style lang="stylus" scoped>
+#dino-scene
+  position relative
+  width 600px
+  height 200px
+  border solid 1px #ddd
+#dino-player
+  position absolute
+  width 88px
+  height 94px
+  bottom 20px
+  left 40px
+</style>
---
+++ src/views/games/Frame.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="game-frame" :style="{
+    'background-image': `url(${asset})`
+  }" />
+</template>
+
+<script>
+export default {
+  name: 'Frame',
+  props: ['asset']
+}
+</script>
+
+<style lang="stylus" scoped>
+.game-frame
+  position absolute
+  top 0
+  left 0
+  width 100%
+  height 100%
+</style>
